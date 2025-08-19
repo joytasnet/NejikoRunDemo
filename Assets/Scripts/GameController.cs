e4aab27035a2a7cc94e772daacd2f83652004bf1
mjpuin <mjpurin@gmail.com>
2025-08-19 09:32:19 +0900

    完成

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -12,21 +12,28 @@ public class GameController : MonoBehaviour
     public LifePanel lifePanel;
     void Update()
     {
-        int score= CalcScore();
-        scoreText.text=$"Score : {score}m";
+        int score = CalcScore();
+        scoreText.text = $"Score : {score}m";
         lifePanel.UpdateLife(nejiko.Life());
 
-        if(nejiko.Life() <= 0){
+        if (nejiko.Life() <= 0)
+        {
             enabled = false;
+            if (PlayerPrefs.GetInt("HighScore") < score)
+            {
+                PlayerPrefs.SetInt("HighScore", score);
+            }
 
-            Invoke("ReturnToTitle",2.0f);
+            Invoke("ReturnToTitle", 2.0f);
         }
-        
+
     }
-    int CalcScore(){
+    int CalcScore()
+    {
         return (int)nejiko.transform.position.z;
     }
-    void ReturnToTitle(){
+    void ReturnToTitle()
+    {
         SceneManager.LoadScene("Title");
     }
 }
--- Assets/Scripts/TitleController.cs
+++ Assets/Scripts/TitleController.cs
@@ -2,9 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 public class TitleController : MonoBehaviour
 {
-    public void OnStartButtonClicked(){
+    public TextMeshProUGUI highScoreText;
+    void Start()
+    {
+        highScoreText.text = $"High Score : {PlayerPrefs.GetInt("HighScore")}m";
+    }
+    public void OnStartButtonClicked()
+    {
         SceneManager.LoadScene("Main");
     }
 }
